029d7b32622f1ae3dcdb96a867e74409b60a7335
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-03-20 20:47:55 -0500

    Import/export modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import moviesData from './moviesData';
 import './App.css';
+import Movies from './Movies';
+import Card from './Card';
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main'>
+      <h1>Rancid Tomatillos</h1>
+    </main>
+    
   );
 }
 
--- src/moviesData.js
+++ src/moviesData.js
@@ -1,4 +1,4 @@
-const movieData = {
+const moviesData = {
     "movies": [
       {
         "id": 694919,
@@ -323,4 +323,4 @@ const movieData = {
     ]
   };
   
-  export default movieData;
+  export default moviesData;
