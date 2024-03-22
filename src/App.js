8d7e95e77d48b300829675cf486a26a9039c5628
Cory Sanders <corysanders3@yahoo.com>
2024-03-22 15:40:59 -0600

    Fetch movies from movies api.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import moviesData from './moviesData';
 import './App.css';
 import Movies from './Movies';
 import SingleMovie from './SingleMovie';
 
 function App() {
-  const [movies, setMovies] = useState(moviesData);
+  const [movies, setMovies] = useState([]);
   const [selection, setSelection] = useState('');
+  const [error, setError] = useState('');
+
+  function fetchMovies() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('There was an error getting the information')
+        }
+        return response.json()
+      })
+      .then(data => setMovies(data.movies))
+      .catch(err => setError(err.message))
+  }
+
+    useEffect(() => {
+      fetchMovies();
+    }, []);
 
   function displayMovie(id) {
     console.log(`displayMovie ${id}`)
@@ -16,6 +33,7 @@ function App() {
   return (
     <main className='main'>
       <h1 className='heading'>Rancid Tomatillos</h1>
+      { error && <h2 className='heading'>{error}</h2> }
       { !selection ? 
       <>
         <h2 className='heading2'>Top Movies Now...</h2>
--- src/Movies.js
+++ src/Movies.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 function Movies({ movies, setSelection, displayMovie }) {
     const movieCards = 
-    movies.movies.map(movie => {
+    movies.map(movie => {
         return (
             <Card
                 title={movie.title}
