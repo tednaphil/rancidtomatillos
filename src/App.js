ad4dd036f745410b80b96edb0aac4685665e93eb
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-03-21 21:48:08 -0500

    Add pointer cursor to button hover

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ function App() {
   return (
     <main className='main'>
       <h1 className='heading'>Rancid Tomatillos</h1>
-      { !selection ? <><h2 className='heading2'>Top Movies Now...</h2><Movies movies={movies} setSelection={setSelection} displayMovie={displayMovie}/></> : <SingleMovie selection={selection} setSelection={setSelection} /> }
+      { !selection ? 
+      <>
+        <h2 className='heading2'>Top Movies Now...</h2>
+        <Movies movies={movies} setSelection={setSelection} displayMovie={displayMovie}/>
+      </>
+      :
+      <SingleMovie selection={selection} setSelection={setSelection} /> }
     </main>
     
   );
