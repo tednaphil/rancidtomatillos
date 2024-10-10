8b6f8bca997c54fc5da2f972cd70c34a35af07fc
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-10-10 14:26:52 -0500

    Refactor SingleMovie component with new fetchSingleMovie request

--- src/APICalls.js
+++ src/APICalls.js
@@ -7,5 +7,17 @@ export const fetchMovies = async () => {
         return await response.json();
     } catch (error) {
         throw error
-    };
+    }
+};
+
+export const fetchSingleMovie = async (movieId) => {
+    try {
+        const response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`);
+        if(!response.ok) {
+            throw new Error ('We couldn\'t find the movie, please check back later.')
+        }
+        return await response.json();
+    } catch (error) {
+        throw error
+    }
 }
