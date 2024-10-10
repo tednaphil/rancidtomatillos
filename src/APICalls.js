71db19304dbe847e328969c1dbf257d3e642d9b6
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-10-10 15:01:44 -0500

    Remove unused fetch call

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,15 +1,3 @@
-export const fetchMovies = async () => {
-    try {
-        const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies');
-        if(!response.ok) {
-            throw new Error(`There was an issue getting the information... check back later. ${response.status}`)
-        }
-        return await response.json();
-    } catch (error) {
-        throw error
-    }
-};
-
 export const fetchSingleMovie = async (movieId) => {
     try {
         const response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import Movies from "./Movies";
 import SingleMovie from "./SingleMovie";
 import { Routes, Route } from "react-router-dom";
 import BadUrl from "./BadUrl";
-import { fetchMovies } from "./APICalls";
 import { QueryClient, QueryClientProvider, useQuery} from "react-query";
 
 const queryClient = new QueryClient();
