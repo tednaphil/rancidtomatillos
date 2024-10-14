41685d26647c1487f840e68ff1036a36bda77caf
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-10-14 11:45:34 -0500

    Refactor App component with updated React Query

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import Movies from "./Movies";
 import SingleMovie from "./SingleMovie";
 import { Routes, Route } from "react-router-dom";
 import BadUrl from "./BadUrl";
-import { QueryClient, QueryClientProvider, useQuery} from "react-query";
+import { useQuery } from "react-query";
+import axios from "axios";
 
-const queryClient = new QueryClient();
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>
-  )
+const retrieveMovies = async () => {
+  const response = await axios.get('https://rancid-tomatillos.herokuapp.com/api/v2/movies');
+  return response.data
 }
 
-function Home() {
-  const [movies, setMovies] = useState([]);
-  const { isPending, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>
-      fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then((res) =>
-        res.json().then((info) => organizeMovieData(info)),
-      ),
-  });
-
-  if (isPending) return 'Loading...'
+function App() {
+  const {
+    data: movies,
+    error,
+    isLoading,
+  } = useQuery('moviesData', retrieveMovies);
+  
+  if (isLoading) return <h2>Loading...</h2>
 
   if (error) return <h2 className="error">{`There was an issue getting the information... check back later.`}</h2>
 
-  function organizeMovieData({ movies }) {
-    const orgMovies = movies.map((movie) => {
+  function organizeMovieData({movies}) {
+    return movies.map((movie) => {
       let movieYear = movie.release_date.slice(0, 4);
       let movieMonth = movie.release_date.slice(5, 7);
       let movieDay = movie.release_date.slice(8, 10);
-      movie.release_date = movieMonth + "/" + movieDay + "/" + movieYear;
-      return movie;
+      const newMovie = {...movie};
+      newMovie.release_date = movieMonth + "/" + movieDay + "/" + movieYear;
+      return newMovie
     });
-    setMovies(orgMovies);
   };
+
+  const orgMovies = organizeMovieData(movies);
 
   return (
     <main className="main">
       <h1 className="heading">Rancid Tomatillos</h1>
       <Routes>
-        <Route path="/" element={<Movies movies={movies} />}></Route>
+        <Route path="/" element={<Movies movies={orgMovies} />}></Route>
         <Route path="/movie/:movieId" element={<SingleMovie />}></Route>
         <Route path="*" element={<BadUrl />}></Route>
       </Routes>
